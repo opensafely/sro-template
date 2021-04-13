40d98cc8ef89196ee9a988bd4772c39270001605
LFISHER7 <lhf282@live.co.uk>
2021-04-13 10:17:37 +0100

    measure output

--- analysis/config.py
+++ analysis/config.py
@@ -1,6 +1,6 @@
 start_date = "2020-12-01"
 end_date = "2021-12-01"
-demographics = ["sex", "age_band", "region", "imd", "ethnicity"]
+demographics = ["sex", "age_band", "region", "imd"]
 marker="SMR"
 codelist_code_column="code"
 codelist_term_column='term'
