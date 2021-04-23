982b5e9212dd81fd1e12fbc78dd2700cb400796c
LFISHER7 <lhf282@live.co.uk>
2021-04-23 11:16:04 +0100

    import specified dates practice study def

--- analysis/study_definition_practice_count.py
+++ analysis/study_definition_practice_count.py
@@ -13,23 +13,22 @@ from cohortextractor import (
 from codelists import *
 from datetime import date
 
-
-start_date = "2020-12-07"
-end_date = "2021-02-01"
+from config import start_date, end_date
 # Specifiy study definition
 
 study = StudyDefinition(
+    index_date=start_date,
     default_expectations={
         "date": {"earliest": start_date, "latest": end_date},
         "rate": "exponential_increase",
         "incidence": 0.1,
     },
 
-    population=patients.registered_as_of(start_date),
+    population=patients.registered_as_of("index_date"),
 
 
     practice=patients.registered_practice_as_of(
-        start_date,
+        "index_date",
         returning="pseudo_id",
         return_expectations={
             "int": {"distribution": "normal", "mean": 25, "stddev": 5}, "incidence": 0.5}
