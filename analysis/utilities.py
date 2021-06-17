caeeec99b9ba0a7a109d139c772f82f8c7493046
LFISHER7 <louis.fisher@thedatalab.org>
2021-06-17 16:59:59 +0100

    reformat and test utility functions

--- analysis/utilities.py
+++ analysis/utilities.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import json
-import datetime
-from dateutil.relativedelta import relativedelta
 import os
-from urllib.request import urlopen
-from pandas.api.types import is_numeric_dtype
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parents[1]
+OUTPUT_DIR = BASE_DIR / "output"
 
 def calculate_imd_group(df, disease_column, rate_column):
     """Converts imd column from ordinal to quantiles and groups by these quintiles.
@@ -101,21 +99,99 @@ def calculate_rate(df, numerator, denominator, rate_per=1000):
     
     return df
         
-def drop_irrelevant_practices(df):
-    #drop practices that do not use the code
-    mean_value_df = df.groupby("practice")["value"].mean().reset_index()
+def drop_irrelevant_practices(df, practice_col):
+    """Drops irrelevant practices from the given measure table.
+    An irrelevant practice has zero events during the study period.
+    Args:
+        df: A measure table.
+        practice_col: column name of practice column
+    Returns:
+        A copy of the given measure table with irrelevant practices dropped.
+    """
+    is_relevant = df.groupby(practice_col).value.any()
+    return df[df[practice_col].isin(is_relevant[is_relevant == True].index)]
 
-    practices_to_drop = list(
-        mean_value_df['practice'][mean_value_df['value'] == 0])
 
-    #drop
-    df = df[~df['practice'].isin(practices_to_drop)]
+def create_child_table(
+    df, code_df, code_column, term_column, measure, nrows=5
+):
+    """
+    Args:
+        df: A measure table.
+        code_df: A codelist table.
+        code_column: The name of the code column in the codelist table.
+        term_column: The name of the term column in the codelist table.
+        measure: The measure ID.
+        nrows: The number of rows to display.
+    Returns:
+        A table of the top `nrows` codes.
+    """
+    event_counts = (
+        df.groupby(f"{measure}_event_code")[f"{measure}"]
+        .sum()  # We can't use .count() because the measure column contains zeros.
+        .rename_axis(code_column)
+        .rename("Events")
+        .reset_index()
+        .sort_values("Events", ascending=False)
+    )
 
-    return df
+    event_counts["Events (thousands)"] = event_counts["Events"] / 1000
 
+    # Gets the human-friendly description of the code for the given row
+    # e.g. "Systolic blood pressure".
+    code_df = code_df.set_index(code_column).rename(
+        columns={term_column: "Description"}
+    )
+    event_counts = (
+        event_counts.set_index(code_column).join(code_df).reset_index()
+    )
+
+    # Cast the code to an integer.
+    event_counts[code_column] = event_counts[code_column].astype(int)
+
+    # return top n rows
+    return event_counts.iloc[:nrows, :]
+
+def get_number_practices(df):
+    """Gets the number of practices in the given measure table.
+    Args:
+        df: A measure table.
+    """
+    return len(df.practice.unique())
+
+
+def get_percentage_practices(measure_table):
+    """Gets the percentage of practices in the given measure table.
+    Args:
+        measure_table: A measure table.
+    """
+
+    # Read in all input practice count files and get num unique
+    practice_df_list = []
+    for file in os.listdir(OUTPUT_DIR):
+        if file.startswith("input_practice_count"):
+            df = pd.read_csv(os.path.join(OUTPUT_DIR, file))
+            practice_df_list.append(df)
+
+    total_practices_df = pd.concat(practice_df_list, axis=0)
+    num_practices_total= get_number_practices(total_practices_df)
+
+    # Get number of practices in measure
+    num_practices_in_study = get_number_practices(measure_table)
+
+    return np.round((num_practices_in_study / num_practices_total) * 100, 2)
 
 def plot_measures(df, title, column_to_plot, category=False, y_label='Rate per 1000'):
+    """Produce time series plot from measures table.  One line is plotted for each sub
+    category within the category column.
 
+    Args:
+        df: A measure table
+        title: Plot title string
+        column_to_plot: Name of column to plot
+        category: Name of column indicating different categories
+        y_label: String indicating y axis text
+    """
     if category:
         for unique_category in df[category].unique():
 
@@ -137,107 +213,5 @@ def plot_measures(df, title, column_to_plot, category=False, y_label='Rate per 1
     else:
         pass
 
-
     plt.show()
     plt.clf()
-
-
-def get_child_codes(df):
-
-    event_code_column = 'event_code'
-    event_column = 'event'
-
-    counts = df.groupby(event_code_column)[event_column].sum()
-    code_dict = dict(counts)
-
-    return code_dict
-
-def create_child_table(df, code_df, code_column, term_column, nrows=5):
-    #pass in df from data_dict
-    #code df contains first digits and descriptions
-
-    #get codes counts
-    code_dict = get_child_codes(df)
-
-    #make df of events for each subcode
-    df = pd.DataFrame.from_dict(
-        code_dict, orient="index", columns=["Events"])
-    df[code_column] = df.index
-
-    #convert snomed
-    if is_numeric_dtype(df[code_column]):
-        
-      
-        df = df.astype({code_column: 'int64'})
-        df.reset_index(drop=True, inplace=True)
-
-    #convert events to events/thousand
-    df['Events (thousands)'] = df['Events'].apply(lambda x: x/1000)
-    df.drop(columns=['Events'])
-
-    #order by events
-    df.sort_values(by='Events (thousands)', inplace=True)
-    df = df.iloc[:, [1, 0, 2]]
-
-    #get description for each code
-
-    def get_description(row):
-        code = row[code_column]
-
-        description = code_df[code_df[code_column]
-                              == code][term_column].values[0]
-
-        return description
-
-    df['Description'] = df.apply(
-        lambda row: get_description(row), axis=1)
-
-    #return top n rows
-    return df.iloc[:nrows, :]
-
-
-def get_number_practices(df, end_date):
-    
-    num_practices_total = len(np.unique(df['practice']))
-
-    end_date = datetime.datetime.strptime(
-        end_date, '%Y-%m-%d')
-
-
-    year_before = end_date - relativedelta(years=1)
-    months_3_before = end_date - relativedelta(months=3)
-
-    df_subset_year = df[df['date'] > year_before]
-    num_practices_year = len(np.unique(df_subset_year['practice']))
-
-    df_subset_months_3 = df[df['date'] > months_3_before]
-    num_practices_months_3 = len(np.unique(df_subset_months_3['practice']))
-
-    numbers_dict = {"total": num_practices_total, "year": num_practices_year, "3_months": num_practices_months_3}
-    return numbers_dict
-
-def calculate_statistics_practices(df, practice_df, end_date):
-
-    
-    num_practices = len(np.unique(practice_df['practice']))
-
-
-    # calculate number of unique practices and caluclate as % of total
-    practices_included_total = get_number_practices(df, end_date)['total']
-    practices_included_year = get_number_practices(df, end_date)['year']
-    practices_included_months_3 = get_number_practices(df, end_date)['3_months']
-
-
-    practices_included_percent_total = float(
-        f'{((practices_included_total/num_practices)*100):.2f}')
-    
-    practices_included_percent_year = float(
-        f'{((practices_included_year/num_practices)*100):.2f}')
-
-    practices_included_percent_months_3 = float(
-        f'{((practices_included_months_3/num_practices)*100):.2f}')
-
-
-    return {"total": {"number": practices_included_total, "percent": practices_included_percent_total}, "year": {"number": practices_included_year, "percent": practices_included_percent_year}, "months_3": {"number": practices_included_months_3, "percent": practices_included_percent_months_3}}
-    
-
