6be399f191e15c84c140bdf9526e726a1d21acec
LFISHER7 <louis.fisher@thedatalab.org>
2021-06-23 11:40:04 +0100

    remove pop codelist

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,10 +1,8 @@
 from cohortextractor import codelist_from_csv
-from config import codelist_path, population_path
+from config import codelist_path
 
 # Change the path of the codelist to your chosen codelist
 codelist = codelist_from_csv(codelist_path, system='snomed')
-
-population_codelist = codelist_from_csv(population_path, system='snomed')
 
 ethnicity_codes = codelist_from_csv(
         "codelists/opensafely-ethnicity.csv",
