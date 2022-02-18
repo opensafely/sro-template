aa31da42842284d58b78f69b0763bed533cee32d
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-02-18 18:14:30 +0000

    refactor: don't output uneccessary outputs

--- analysis/plot_measures.py
+++ analysis/plot_measures.py
@@ -3,60 +3,17 @@ from pathlib import Path
 import pandas as pd
 import os
 from cohortextractor import Measure
+from study_definition import measures
 from config import demographics, codelist_path
 from ebmdatalab import charts
 
 BASE_DIR = Path(__file__).parents[1]
 OUTPUT_DIR = BASE_DIR / "output"
 
-# Create default measures
-measures = [
-
-    Measure(
-        id="event_code_rate",
-        numerator="event",
-        denominator="population",
-        group_by=["event_code"]
-    ),
-
-    Measure(
-        id="practice_rate",
-        numerator="event",
-        denominator="population",
-        group_by=["practice"]
-    ),
-
-    Measure(
-        id="population_rate",
-        numerator="event",
-        denominator="population",
-        group_by="population",
-        small_number_suppression=False
-    )
-
-
-]
-
-#Add demographics measures
-
-for d in demographics:
- 
-    m = Measure(
-        id=f'{d}_rate',
-        numerator="event",
-        denominator="population",
-        group_by=[d]
-    )
-    
-    measures.append(m)
-
-
-
 measures_dict = {}
 
 for m in measures:
     measures_dict[m.id] = m
-
 
 for key, value in measures_dict.items():
     
@@ -80,15 +37,14 @@ for key, value in measures_dict.items():
         ).savefig('output/decile_chart.png', bbox_inches='tight')  
         
     elif value.id=='event_code_rate':
-        df.to_csv(os.path.join(OUTPUT_DIR, f'rate_table_{value.group_by[0]}.csv'), index=False)
         codelist = pd.read_csv(codelist_path)
         child_code_table = create_child_table(df=df, code_df=codelist, code_column='code', term_column='term')
         child_code_table.to_csv('output/child_code_table.csv', index=False)
 
     elif value.id=='population_rate':
         plot_measures(df, filename=f'plot_{value.group_by[0]}.png', title=f'Breakdown by {value.group_by[0]}', column_to_plot='value', category=False, y_label='Proportion')
-        df.to_csv(os.path.join(OUTPUT_DIR, f'rate_table_{value.group_by[0]}.csv'), index=False)
+        
 
     else:
         plot_measures(df, filename=f'plot_{value.group_by[0]}.png', title=f'Breakdown by {value.group_by[0]}', column_to_plot='value', category=value.group_by[0], y_label='Proportion')
-        df.to_csv(os.path.join(OUTPUT_DIR, f'rate_table_{value.group_by[0]}.csv'), index=False)
+        
