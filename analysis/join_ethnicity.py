e8db57fc9a8ec5f31f1f5431003e7625f6ca2b64
Iain Dillingham <iain@dillingham.me.uk>
2022-03-03 11:42:17 +0000

    Drop the index when writing the CSV file
    The index is a Pandas RangeIndex [1], which implements a monotonic
    integer range and was added when the CSV file was read; it wasn't added
    when the CSV file was written by cohort-extractor. By dropping it when
    writing the CSV file, we ensure the CSV file has the same shape as a CSV
    file that was written by cohort-extractor.
    
    [1]: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.RangeIndex.html

--- analysis/join_ethnicity.py
+++ analysis/join_ethnicity.py
@@ -13,4 +13,4 @@ for file in os.listdir('output'):
             df = pd.read_csv(file_path)
             merged_df = df.merge(ethnicity_df, how='left', on='patient_id')
             
-            merged_df.to_csv(file_path)
+            merged_df.to_csv(file_path, index=False)
